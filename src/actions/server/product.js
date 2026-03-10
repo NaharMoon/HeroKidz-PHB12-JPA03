41b7bcc693da3b7f045e51de7615c6b2e389e400
Nahar Moon <naharmoon2003@gmail.com>
2026-03-10 12:43:55 +0600

    fix vercel build

--- src/actions/server/product.js
+++ src/actions/server/product.js
@@ -3,18 +3,44 @@
 import { collections, dbConnect } from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
 
+// export const getProducts = async () => {
+//   const products = await dbConnect(collections.PRODUCTS).find().toArray();
+//   return products;
+// };
+
 export const getProducts = async () => {
-  const products = await dbConnect(collections.PRODUCTS).find().toArray();
-  return products;
+  try {
+    const products = await dbConnect(collections.PRODUCTS).find().toArray();
+    return products;
+  } catch (error) {
+    console.log("DB error ignored during build");
+    return [];
+  }
 };
 
+// export const getSingleProduct = async (id) => {
+//   if (id.length != 24) {
+//     return {};
+//   }
+//   const query = { _id: new ObjectId(id) };
+
+//   const product = await dbConnect(collections.PRODUCTS).findOne(query);
+
+//   return { ...product, _id: product._id.toString() } || {};
+// };
+
 export const getSingleProduct = async (id) => {
-  if (id.length != 24) {
+  try {
+    if (id.length !== 24) {
+      return {};
+    }
+
+    const query = { _id: new ObjectId(id) };
+    const product = await dbConnect(collections.PRODUCTS).findOne(query);
+
+    return { ...product, _id: product._id.toString() } || {};
+  } catch (error) {
+    console.log("DB error ignored during build");
     return {};
   }
-  const query = { _id: new ObjectId(id) };
-
-  const product = await dbConnect(collections.PRODUCTS).findOne(query);
-
-  return { ...product, _id: product._id.toString() } || {};
 };
